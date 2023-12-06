0bce032baafd42a7948f510967e437cd645cb227
m-buschmann <snep114@gmail.com>
2023-12-06 12:51:45 +0100

    delete double tags

--- recommender-base/read_data.py
+++ recommender-base/read_data.py
@@ -1,62 +1,16 @@
 import csv
 from sqlalchemy.exc import IntegrityError
-from models import Movie, MovieGenre, Tags
+from models import db, Movie, MovieGenre, Tags
+from flask import Flask
 
-"""def check_and_read_data(db):
-    # check if we have movies in the database
-    # read data if database is empty
-    if Movie.query.count() == 0:
-        # read movies from csv
-        with open('data/movies.csv', newline='', encoding='utf8') as csvfile:
-            reader = csv.reader(csvfile, delimiter=',')
-            count = 0
-            for row in reader:
-                if count > 0:
-                    try:
-                        id = row[0]
-                        title = row[1]
-                        movie = Movie(id=id, title=title)
-                        db.session.add(movie)
-                        genres = row[2].split('|')  # genres is a list of genres
-                        for genre in genres:  # add each genre to the movie_genre table
-                            movie_genre = MovieGenre(movie_id=id, genre=genre)
-                            db.session.add(movie_genre)
-                        db.session.commit()  # save data to database
-                    except IntegrityError:
-                        print("Ignoring duplicate movie: " + title)
-                        db.session.rollback()
-                        pass
-                count += 1
-                if count % 100 == 0:
-                    print(count, " movies read")
-
-#def check_tags(db):
-    #TODO: evl sortieren welche tags wir ausgeben wollen, nicht nur den ersten
-    if Tags.query.count() == 0:
-        # read tags from csv
-        with open('data/tags.csv', newline='', encoding='utf8') as csvfile:
-            reader = csv.reader(csvfile, delimiter=',')
-            count = 0
-            for row in reader:
-                if count > 0:
-                    try:
-                        id = row[1]
-                        tag = row[2]
-                        tags = Tags(id=id, tag=tag)
-                        db.session.add(tags)
-                        db.session.commit()  # save data to database
-                    except IntegrityError:
-                        #print("Ignoring duplicate movieID: " + id)
-                        db.session.rollback()
-                        pass
-                count += 1
-                if count % 100 == 0:
-                    print(count, " tags read")
-    count_tags = Tags.query.count()
-    print(f"Number of entries in the Tags table: {count_tags}")"""
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///movie_recommender.sqlite'  # Update with your database URI
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db.init_app(app)
 
 
 
+#TODO: evl sortieren welche tags wir ausgeben wollen
 def check_and_read_data(db):
     # check if we have movies in the database
     # read data if database is empty
@@ -90,9 +44,11 @@ def check_and_read_data(db):
                                     tags_list = tags.split(',')
 
                                     for tag in tags_list:
-                                        n_tag = Tags(movie_id=movie_id, tag=tag.strip())
-                                        db.session.add(n_tag)
-                                        
+                                        existing_tag = Tags.query.filter_by(movie_id=movie_id, tag=tag.strip()).first() #check that we have not doublicates
+                                        if not existing_tag:
+                                            n_tag = Tags(movie_id=movie_id, tag=tag.strip())
+                                            db.session.add(n_tag)
+
                         db.session.commit()  # save data to database
                     except IntegrityError:
                         print("Ignoring duplicate movie: " + title)
@@ -101,16 +57,6 @@ def check_and_read_data(db):
                 count += 1
                 if count % 100 == 0:
                     print(count, " movies read")
-
-import csv
-from flask import Flask
-from sqlalchemy.exc import IntegrityError
-from models import db, Movie, MovieGenre, Tags
-
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///movie_recommender.sqlite'  # Update with your database URI
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db.init_app(app)
 
 
 if __name__ == '__main__':
