980f8054ec928d11df723d5bb0745f4e8d8bfbf1
m-buschmann <snep114@gmail.com>
2024-01-08 14:38:53 +0100

    6

--- recommender.py
+++ recommender.py
@@ -156,12 +156,12 @@ def recommendations():
 
     recom_idx = loaded_model.predict_for_user(user, movies) # get recommendations for the current user
     recom_idx = recom_idx.sort_values(ascending=False)[:RECOMMENDATIONS]#sort the series by value#
-    print(recom_idx[1][1])
+
     mov_links = db.session.query(Movie, MovieLinks).join(MovieLinks, (Movie.id == MovieLinks.movie_id)) #get the movies from the database
 
     recom = [mov_links.filter(Movie.id == idx).first() for idx in recom_idx.index]
 
-    return render_template("recommendations.html", movies = recom, db = db, user = current_user.id, Ratings = Ratings, MovieTags = MovieTags)
+    return render_template("recommendations.html", movies = recom, db = db, user = current_user.id, Ratings = Ratings, MovieTags = MovieTags, percentage = recom_idx)
 
 @app.route('/my_ratings')
 @login_required  # User must be authenticated
