370d76b8a22c1110304615f5dfb660650ec95cb2
m-buschmann <snep114@gmail.com>
2024-01-08 13:12:20 +0100

    .

--- recommender.py
+++ recommender.py
@@ -85,7 +85,7 @@ def fitBPR_command():
 @app.route('/')
 def home_page():
     # render home.html template
-    return render_template("home.html", app_name = user_manager.USER_APP_NAME)
+    return render_template("home.html")
 
 
 # The Members page is only accessible to authenticated users via the @login_required decorator
