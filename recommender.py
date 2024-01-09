16c651d0f975e2d64d98c6b816ad0b3649b4b9bb
LAPTOP-C24L6L0H\j k <jkohler@uni-osnabrueck.de>
2024-01-09 19:17:07 +0100

    i claim no ownership of this beaty

--- recommender.py
+++ recommender.py
@@ -30,7 +30,7 @@ class ConfigClass(object):
     SQLALCHEMY_TRACK_MODIFICATIONS = False  # Avoids SQLAlchemy warning
 
     # Flask-User settings
-    USER_APP_NAME = "Movie Recommender"  # Shown in and email templates and page footers
+    USER_APP_NAME = "Movie Ratommender"  # Shown in and email templates and page footers
     USER_ENABLE_EMAIL = False  # Disable email authentication
     USER_ENABLE_USERNAME = True  # Enable username authentication
     USER_REQUIRE_RETYPE_PASSWORD = True  # Simplify register form
